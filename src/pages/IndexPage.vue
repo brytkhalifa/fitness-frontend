50f9b72902c104a9b2e17488ab2f7cd1e2dadc55
Melissa <melissa-mungo@hotmail.de>
2023-09-15 23:28:14 +0200

    addes Login Page Component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,6 +7,11 @@
       :meta="meta"
     ></example-component>
   </q-page>
+
+  <div>
+    Hello world
+    {{ props.x }}
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,26 +22,30 @@ import { ref } from 'vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'ct1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
+});
+
+const props = defineProps({
+  x: { type: String, default: '10' },
 });
 </script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <div class="row justify-center" style="height: 250px">
+    <div class="col-6 self-center">
+      <h3 style="color: pink">Letz Fit</h3>
+    </div>
+  </div>-->
+
+ <div class="column"  style="height: 150px">
+    <div class="row self-center" ><h3 style="color:pink;">Letz Fit</h3></div>
+
+    <div class="col self-center" style="width: 300px">
+
+      <div class="q-pa-md" style="padding-top: 150px">
+        <div class="q-gutter-md" style="max-width: 300px">
+          <div class="row">
+            <p style="border-radius: 50%; width: 100px; height: 100px; background:pink"></p>
+          <p style="float: left; width: 150px; height: 150px; margin: 20px; shape-outside: circle(50% at 30%); clip-path: circle(50% at 0%); background: lightblue;"></p>
+          </div>
+
+          <q-input
+            color="purple-9"
+            v-model="benutzername"
+            label="Benutzername"
+          />
+          <q-input
+            :type="visible ? 'text' : 'password'"
+            color="purple-9"
+            v-model="passwort"
+            label="Passwort"
+          />
+          <q-toggle
+            label="See password"
+            v-model="visible"
+          />
+          <q-btn style="background: #ff0080; color: white" label="Login" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const benutzername = ref('');
+const passwort = ref('');
+const visible = ref(false);
+</script>
+<style lang="scss">
+p {
+  background-color: #000000;
+}
+
+</style>
