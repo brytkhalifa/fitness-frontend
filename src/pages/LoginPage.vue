e81a175c506ce70d84393e86d9c47d19f821f1e1
Bright Baah <brytkhalifa@gmail.com>
2023-09-17 11:33:44 +0200

    login page structure

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,50 +4,84 @@
       <h3 style="color: pink">Letz Fit</h3>
     </div>
   </div>-->
-
- <div class="column"  style="height: 150px">
-    <div class="row self-center" ><h3 style="color:pink;">Letz Fit</h3></div>
-
-    <div class="col self-center" style="width: 300px">
-
-      <div class="q-pa-md" style="padding-top: 150px">
-        <div class="q-gutter-md" style="max-width: 300px">
-          <div class="row">
-            <p style="border-radius: 50%; width: 100px; height: 100px; background:pink"></p>
-          <p style="float: left; width: 150px; height: 150px; margin: 20px; shape-outside: circle(50% at 30%); clip-path: circle(50% at 0%); background: lightblue;"></p>
+  <div class="row justify-center items-center">
+    <div class="col-10 col-md-8 q-mt-xl ">
+      <!-- full form-->
+      <div class="row items-center">
+        <!-- LEFT SIDE -->
+        <div class="col-12 col-md-6">
+          <div class="as row text-center items-center justify-center ">
+            <div class="as col-4">
+              <q-toolbar-title class="bg-white text-pink text-h3 " style="font-family: 'Lobster', cursive">
+                Letz Fit
+              </q-toolbar-title>
+            </div>
+            <div class="as col-12">
+              <div class="row justify-center">
+                <div class="col-7">
+                  <q-img
+                    fit="fill"
+                    placeholder-src="https://img.freepik.com/free-photo/big-dumbbells-white_144627-24203.jpg?size=626&ext=jpg"
+                    src="https://img.freepik.com/free-photo/big-dumbbells-white_144627-24203.jpg?size=626&ext=jpg"
+                    spinner-color="white">
+                  </q-img>
+                </div>
+              </div>
+              <!--  -->
+            </div>
           </div>
-
-          <q-input
-            color="purple-9"
-            v-model="benutzername"
-            label="Benutzername"
-          />
-          <q-input
-            :type="visible ? 'text' : 'password'"
-            color="purple-9"
-            v-model="passwort"
-            label="Passwort"
-          />
-          <q-toggle
-            label="See password"
-            v-model="visible"
-          />
-          <q-btn style="background: #ff0080; color: white" label="Login" />
+        </div>
+        <!-- RIGHT SIDE -->
+        <div class="as col-12 col-md-3 col-lg-3">
+          <q-form
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-md"
+          >
+            <q-input
+              color="purple-9"
+              v-model="benutzername"
+              label="Benutzername"
+            />
+            <q-input
+              :type="visible ? 'text' : 'password'"
+              color="purple-9"
+              v-model="passwort"
+              label="Passwort"
+            />
+            <q-toggle
+              label="See password"
+              v-model="visible"
+            />
+            <q-btn @click.stop.prevent="click" type="submit" style="background: #ff0080; color: white" label="Login"/>
+          </q-form>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import {onMounted, ref} from 'vue';
+import axios from "axios";
+
 
 const benutzername = ref('');
 const passwort = ref('');
 const visible = ref(false);
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:8000/api/test');
+})
 </script>
 <style lang="scss">
 p {
   background-color: #000000;
 }
+
+/*.as{
+  border-style: dotted;
+  border-width: 1px;
+  border-color: blue;
+}*/
 
 </style>
